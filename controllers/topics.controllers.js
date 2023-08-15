848e8b91fe467130c49dcf96b449211831afbcdf
usmosis <usman_ali7@outlook.com>
2023-08-15 10:17:01 +0100

    completes task 3 with endpoints JSON

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,5 +1,10 @@
 const {selectTopics} = require("../models/topics.models.js")
 
+const endPoints = require("../endpoints.json")
 exports.sendTopics = (req, res) => {
     selectTopics().then((topics) => res.status(200).send({topics}))
+}
+
+exports.sendEndpoints = (req, res) => {
+    return res.status(200).send({endPoints});
 }
