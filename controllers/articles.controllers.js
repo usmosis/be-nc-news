7981718b1713ce6545fcc324c229240b1b6a7bcd
usmosis <usman_ali7@outlook.com>
2023-08-15 12:07:22 +0100

    completes task 4 with error handling 404 & 400

---
+++ controllers/articles.controllers.js
@@ -0,0 +1,10 @@
+const {selectArticleById} = require('../models/articles.models')
+
+exports.sendArticleById = (req, res, next) => {
+    const {article_id} = req.params;
+    selectArticleById(article_id)
+    .then((article) => res.status(200).send({article}))
+    .catch((err) => {
+        next(err)
+    })
+}
